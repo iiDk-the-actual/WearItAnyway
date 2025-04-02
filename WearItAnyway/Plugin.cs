49f18faa0fd7b40721c0ab2842cf695658089956
iiDk <54154105+iiDk-the-actual@users.noreply.github.com>
2025-04-02 16:45:44 -0400

    Add project files.

---
+++ WearItAnyway/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace WearItAnyway
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+    }
+}
